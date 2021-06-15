71ae7595b4c533569c18f1060d94fac2adcbcd00
Maackk17 <stuemaik@gmail.com>
2021-06-15 15:04:14 +0200

    Initial commit

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Person;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api', name: 'api')]
+    public function index(): Response
+    {
+        //create new instance of entity Person with dummy values
+        $dummy_person = new Person;
+        $dummy_person->setGender('male');
+        $dummy_person->setFirstname('Maik');
+        $dummy_person->setLastname('Stuermer');
+
+        //get data from instance and encode to json
+        $data = ['gender' => $dummy_person->getGender(), 'firstname' => $dummy_person->getFirstname(), 'lastname' => $dummy_person->getLastname()];
+        $json = json_encode($data);
+        
+        //return json to twig template
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+            'json' => $json,
+        ]);
+        
+    }
+}
